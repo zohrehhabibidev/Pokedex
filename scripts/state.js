69612e459ab6cb89b6dcfc336efbba48e3d59ca1
Zohreh Habibi <your-email@example.com>
2026-04-01 10:36:15 +0200

    feat: fetch pokemon list and store in state

--- scripts/state.js
+++ scripts/state.js
@@ -1,1 +1,82 @@
-// app state
+// What is State?
+// State means the current situation of the app.
+// - What data do we have? چه داده‌ای داریم؟
+// - Where is that data stored? آن داده کجا نگه‌داری می‌شود؟
+// - When does it change? چه موقع تغییر می‌کند؟
+// - Who uses it? چه کسی از آن استفاده می‌کند؟
+
+// For example, in a Pokédex app, the program needs to know:
+// - Which Pokémon are already loaded
+// - Are we getting data from the API right now or not
+// - Where to start getting the next data
+// - How many items to get each time
+// If the app doesn’t know these things,
+// it cannot work correctly.
+
+// Why do we need state?
+// Because the app is always changing.
+// Data updates while the app is running,
+// so we need to keep track of it.
+//offset = از کدام نقطه شروع به برگرداندن داده کند(API)مشخص می‌کند
+//loadedPokemons = نگه داشتن همه‌ی پوکمون‌هایی که تا این لحظه لود شده‌اند
+//limit = تعداد آیتمی که در هر درخواست می‌خواهیم بگیریم20
+//isLoading = نشان می‌دهد الان برنامه وسط یک درخواست هست یا نه
+
+
+//TODO
+// only made states for getting the Pokémon list
+let loadedPokemons = [];
+let isLoading = false; // async controller
+let currentOffset = 0;
+let limit = 20;
+
+async function getPokemonsData() {
+  //Dynamic URL for load more
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${currentOffset}`;
+
+  try {
+    //Go get data from the API and Wait for the server to respond
+    const response = await fetch(url);
+    //Convert answers into usable data
+    const pokemonJsonData = await response.json();
+
+    //Just take the list of Pokémon and save it into state.
+    loadedPokemons = pokemonJsonData.results;
+    console.log(loadedPokemons);
+  } catch (error) {
+    console.warn(error);
+  }
+}
+
+getPokemonsData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let selectedPokemon = 0;
+// let currentPokemonIndex = 0;
+// let searchResults = [];
+// let isSearchMode = false;
