53c3d7b182c17b78e807e7b19ca19429a24871c8
Zohreh Habibi <your-email@example.com>
2026-04-01 10:40:38 +0200

    refactor: move fetch logic to api.js

--- scripts/api.js
+++ scripts/api.js
@@ -1,1 +1,21 @@
+//API logic
 // fetch functions
+async function getPokemonsData() {
+  //Dynamic URL for load more
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${currentOffset}`;
+
+  try {
+    //Go get data from the API and Wait for the server to respond
+    const response = await fetch(url);
+    //Convert answers into usable data
+    const pokemonJsonData = await response.json();
+
+    //Just take the list of Pokémon and save it into state.
+    loadedPokemons = pokemonJsonData.results;
+    console.log(loadedPokemons);
+  } catch (error) {
+    console.warn(error);
+  }
+}
+
+getPokemonsData();
--- scripts/state.js
+++ scripts/state.js
@@ -30,25 +30,7 @@ let isLoading = false; // async controller
 let currentOffset = 0;
 let limit = 20;
 
-async function getPokemonsData() {
-  //Dynamic URL for load more
-  const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${currentOffset}`;
 
-  try {
-    //Go get data from the API and Wait for the server to respond
-    const response = await fetch(url);
-    //Convert answers into usable data
-    const pokemonJsonData = await response.json();
-
-    //Just take the list of Pokémon and save it into state.
-    loadedPokemons = pokemonJsonData.results;
-    console.log(loadedPokemons);
-  } catch (error) {
-    console.warn(error);
-  }
-}
-
-getPokemonsData();
 
 
 
