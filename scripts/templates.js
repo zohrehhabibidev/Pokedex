f4804267c40509fcd366a26f4c5cb6c62056db43
Zohreh Habibi <your-email@example.com>
2026-04-02 12:16:21 +0200

    feat: apply dynamic background styles to cards based on pokemon type

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,12 +1,12 @@
 // template strings / HTML builders
-
+//${pok.type} For the background class
 function createPokemonCard(pok) {
   return `
-    <div class="pokemonCard">
+    <div class="pokemonCard ${pok.type}">
       <h3 class="pokemonTitle">${pok.name.toUpperCase()}</h3>
       <img src="${pok.imageUrl}" alt="${pok.name}">
-      <span class="pokemonUrl">${pok.type}</span>
-      <span class="pokemonUrl">${pok.url}</span>
+      <span class="pokemonType">${pok.type}</span>
+      <span class="pokemonUrl" style="display:none">${pok.url}</span>
     </div>
   `;
 }
